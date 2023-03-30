666af9b9a287a56f3f2773708861b0c564a32173
Wilbert Santos <wgs.g92@gmail.com>
2023-03-30 13:29:03 +0800

    ''

---
+++ database/migrations/2023_03_25_034232_create_alerts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->id();
+            $table->string('coin')->default('BTCUSDT');
+            $table->decimal('price', 18, 8)->default(0);;
+            $table->enum('condition', ['lower', 'higher'])->default('higher');
+            $table->enum('status', ['active', 'done'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alerts');
+    }
+};
